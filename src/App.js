75bd06fcce6e929fe8cbc0636ab4be92c0a15677
Nikita <overclocker96@gmail.com>
2023-12-08 01:28:45 +0500

    Добавляются планы

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import './App.css';
-import PlanTable from './components/PlanTable/PlanTable';
-import plus from './assets/Union.png'
+import MainApp from './components/PlanTable/PlanTable';
 
 function App() {
   return (
     <div className="App" id='root'>
-      <header className="App-header">
-        <h1>Make Your Flight Simulation</h1>
-          <form class="add-stage" id="form" method='POST' enctype="application/json"> 
-            <div class="add-stage-element add-stage-element-1">
-              <p class="course-label">Course:</p>
-              <input type="text" class="course" name="course"/>
-            </div>
-            <div class="add-stage-element add-stage-element-2">
-              <p class="speed-label">Indicated Air Speed (m/s):</p>
-              <input type="number" class="speed" step="0.01" name="speed"/>
-            </div>
-            <div class="add-stage-element add-stage-element-3">
-              <p class="altitude-label">True Altitude:</p>
-              <input type="number" class="altitude" name="altitude"/>
-            </div>
-            <div class="add-stage-element add-stage-element-4">
-              <button onClick={(evt) => {evt.preventDefault()} } type="submit" class="plus"><img src={plus}/></button>
-            </div>
-          </form>
-      </header>
-      <main class="main-info">
-        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d69934.32592146858!2d60.723338649999995!3d56.78678595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1700587609013!5m2!1sru!2sru" width="623" height="604" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <div class="info-table">
-          <table class="table">
-            <thead>
-              <div class="table-head">
-                <h2>Flight Plan Stages</h2>
-                <div class="table-head-info">
-                  <tr class="table-element">
-                    <th class="table-element-info table-element-info-1">#</th>
-                    <th class="table-element-info table-element-info-2">Course</th>
-                    <th class="table-element-info table-element-info-3 table-speed">Indicated Air Speed</th>
-                    <th class="table-element-info table-element-info-4">TrueAltitude</th>
-                  </tr>
-                </div>
-              </div>
-            </thead>
-            <tbody>
-              <div class='scroll'>
-                <PlanTable />
-              </div>
-            </tbody>
-          </table>
-        </div>
-      </main>
+      <MainApp />
     </div>
   );
 }
--- src/components/PlanTable/PlanTable.jsx
+++ src/components/PlanTable/PlanTable.jsx
@@ -1,50 +1,95 @@
 import React, {useState} from "react";
-import './PlanTable.css';
 import PlanItem from "../Plan/Plan";
+import plus from '../../assets/Union.png'
+import './PlanTable.css';
 
 
-import minus from '../../assets/Decrease.png'
+const MainApp = () => {
 
+  const [plan, setPlan] = useState([]);
 
-const picturesIdCreator = () => {
-  let previousId = 0;
+  const getData = (form) => {
+    const dataForm = new FormData(form);
+    const data = {};
+    for (let [name, value] of dataForm){
+      data[name] = value;
+    }
+    return data;
+  }
 
-  return () => {
-    previousId+=1;
-    return previousId;
-  };
-};
+  const clearForm = () => {
+    const form = document.getElementById('form');
 
+    form.querySelector('.course').value = '';
+    form.querySelector('.speed').value = '';
+    form.querySelector('.altitude').value = '';
+  }
 
-const PlanTable = () => {
-  const picturesIdGenerator = picturesIdCreator();
-
-  const [plan, setPlan] = useState([
-
-  ])
-
-  const handlerAddPlan = e => {
-    e.preventDefault();
-
+  const handlerAddPlan = ({course, speed, altitude}) => {
+    if (!course || !speed || !altitude){ return }
     setPlan(prev => [...prev, {id: plan.length+1,
-      course: '56.84071, 60.65071', speed: '9999', altitude: '9999'}])
+      course, speed, altitude}])
+
+    clearForm();
   }
 
   return (
     <>
-      {plan && plan.map(element => 
-      <PlanItem 
-        id={element.id}
-        course={element.course}
-        speed={element.speed}
-        altitude={element.altitude}
-      />
-      )}      
-      <button onClick={handlerAddPlan}>
-        Нажми меня
-      </button>
+      <header className="App-header">
+      <h1>Make Your Flight Simulation</h1>
+        <form class="add-stage" id="form" method='POST' enctype="application/json"> 
+          <div class="add-stage-element add-stage-element-1">
+            <p class="course-label">Course:</p>
+            <input type="text" class="course" name="course" required/>
+          </div>
+          <div class="add-stage-element add-stage-element-2">
+            <p class="speed-label">Indicated Air Speed (m/s):</p>
+            <input type="number" class="speed" step="0.01" name="speed" required/>
+          </div>
+          <div class="add-stage-element add-stage-element-3">
+            <p class="altitude-label">True Altitude:</p>
+            <input type="number" class="altitude" name="altitude" required/>
+          </div>
+          <div class="add-stage-element add-stage-element-4">
+            <button onClick={e => {e.preventDefault(); handlerAddPlan(getData(document.getElementById('form')))}} type="submit" class="plus"><img src={plus}/></button>
+          </div>
+        </form>
+    </header>
+    <main class="main-info">
+      <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d69934.32592146858!2d60.723338649999995!3d56.78678595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1700587609013!5m2!1sru!2sru" width="623" height="604" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="info-table">
+        <table class="table">
+          <thead>
+            <div class="table-head">
+              <h2>Flight Plan Stages</h2>
+              <div class="table-head-info">
+                <tr class="table-element">
+                  <th class="table-element-info table-element-info-1">#</th>
+                  <th class="table-element-info table-element-info-2">Course</th>
+                  <th class="table-element-info table-element-info-3 table-speed">Indicated Air Speed</th>
+                  <th class="table-element-info table-element-info-4">TrueAltitude</th>
+                </tr>
+              </div>
+            </div>
+          </thead>
+          <tbody>
+            <div class='scroll'>
+            {plan && plan.map(element => 
+              <PlanItem 
+                key={element.id}
+                id={element.id}
+                course={element.course}
+                speed={element.speed}
+                altitude={element.altitude}
+              />
+            )} 
+            </div>
+          </tbody>
+        </table>
+      </div>
+    </main>     
     </>
   ) 
 }
 
-export default PlanTable;
+export default MainApp;
